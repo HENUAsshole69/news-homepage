109df93876d9161b8a2a537c8e5e40a6e5c1cd65
Administrator <qq844303749@gmail.com>
2020-08-30 22:55:55 +0800

    fixing token not saving

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,20 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import AxiosInstance from "./client/AxiosInstance";
 
 export default Vue.extend({
   name: 'App',
 
   components: {
   },
-
+  async mounted() {
+    if (localStorage.getItem('token') !== null) {
+      AxiosInstance.defaults.headers.common['x-api-key'] = localStorage.getItem('token')
+      this.$store.commit('login', localStorage.getItem('token'))
+      await this.$store.dispatch("getUserObj")
+    }
+  },
   data: () => ({
     //
   }),
