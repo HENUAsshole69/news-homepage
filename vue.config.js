0a1fe63729b0a8d7cdc45751f2975b3619d06088
Administrator <qq844303749@gmail.com>
2020-09-09 23:28:28 +0800

    no splitting for css

--- vue.config.js
+++ vue.config.js
@@ -26,6 +26,21 @@ module.exports = {
                 "window.Quill": "quill/dist/quill.js",
                 Quill: "quill/dist/quill.js"
             })
-        ]
+        ],
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    default: false,
+                    // Merge all the CSS into one file
+                    styles: {
+                        name: 'styles',
+                        test: m => m.constructor.name === 'CssModule',
+                        chunks: 'all',
+                        minChunks: 1,
+                        enforce: true
+                    }
+                },
+            },
+        }
     }
 }
