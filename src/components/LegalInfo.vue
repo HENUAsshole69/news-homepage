7656d905bf6f08de2c518e8fb552d36aa5a5d95c
lu1kaifeng <8302078+lu1kaifeng@user.noreply.gitee.com>
2020-11-24 18:46:40 +0800

    copyright

--- src/components/LegalInfo.vue
+++ src/components/LegalInfo.vue
@@ -8,7 +8,7 @@
                         <a href="javascript:void(0);" style="text-decoration: none;color: rgba(82,82,97,1)">4406441100002018</a>&nbsp;&nbsp; 河南省文物拍卖许可证：
                         <a href="javascript:void(0);" style="text-decoration: none;color: rgba(82,82,97,1)">豫文物证2019001号</a>&nbsp;&nbsp;-->
                       <a href="http://beian.miit.gov.cn/" target="_blank">豫ICP备2020030994号-1</a> |
-                        Copyright 2016 河南国文艺术品经营有限公司
+                        Copyright 2020 河南国文艺术品经营有限公司
 
                     </div>
 
